f77d9826b58747c0d2f90a8dc2daec00f6db3e44
srbalbucio <srbalbuciogf@gmail.com>
2023-11-05 15:13:14 -0300

    Os métodos de join estavam como POST, mas o certo era PUT

--- src/main/java/balbucio/discordoauth/DiscordAPI.java
+++ src/main/java/balbucio/discordoauth/DiscordAPI.java
@@ -75,7 +75,8 @@ public class  DiscordAPI
                 .requestBody(new JSONObject().put("access_token", accessToken).toString())
                 .ignoreContentType(true);
         request.header("Authorization", "Bot " + botToken);
-        request.method(org.jsoup.Connection.Method.PUT);
+        request.header("Content-Type", "application/json")
+        request.method(org.jsoup.Connection.Method.PUT).;
         int s = request.execute().statusCode();
         return s == 201 || s == 204;
     }
@@ -84,7 +85,7 @@ public class  DiscordAPI
         org.jsoup.Connection request = Jsoup.connect(BASE_URI+"/channels/{channel.id}/recipients/{user.id}"
                         .replace("{channel.id}", channel.getID())
                         .replace("{user.id}", user.getId()))
-                .requestBody(new JSONObject().put("access_token", accessToken).toString())
+                .requestBody("{'access_token': '" + accessToken + "'}")
                 .ignoreContentType(true);
         setHeaders(request);
         request.method(org.jsoup.Connection.Method.PUT);
